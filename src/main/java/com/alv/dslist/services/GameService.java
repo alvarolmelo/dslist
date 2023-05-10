edcf34742247dd901b3052ff579b5c97def74c29
Alvaro Vbox-Ma <alvarolmelo@gmail.com>
2023-05-10 20:21:03 -0300

    Get games by lists

--- src/main/java/com/alv/dslist/services/GameService.java
+++ src/main/java/com/alv/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.alv.dslist.services;
 import com.alv.dslist.dto.GameDTO;
 import com.alv.dslist.dto.GameMinDTO;
 import com.alv.dslist.entities.Game;
+import com.alv.dslist.projections.GameMinProjection;
 import com.alv.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,12 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
